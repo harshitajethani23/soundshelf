e939af5b1380ca0e8e5b17a08a11d5be76992ba8
harshitajethani23 <harshitajethani2@gmail.com>
2025-11-14 00:56:58 +0530

    some changes added

--- client/src/components/SongCard.jsx
+++ client/src/components/SongCard.jsx
@@ -15,14 +15,15 @@ function SongCard({_id, title,image,singer,loadSongs}) {
     return (
     <Link to={`/song/${_id}`} 
     className="m-8 shadow-lg w-[350px] rounded-md relative">
+           <img src={image} className=" w-full h-[400px] object-cover rounded-md"/>
+       
         <h2 className="absolute top-3 text-white text-4xl ml-2 ">{title}
             <Trash className="text-blue inline-block absolute top-3 right-3" onClick={(e)=>{deleteSong();
                 e.preventDefault();
                 e.stopPropagation();
             }}/>
         </h2>
-        <img src={image} className=" w-full h-[400px] object-cover rounded-md"/>
-       
+     
         <p className="text-center text-white text-2xl mb-2">singer: {singer}</p>
         </Link>)
 }
